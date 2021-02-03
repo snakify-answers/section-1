90e37055f03a007a48de289e74ba1281737d6bd8
RyanMoodGAMING <ryanmood.098@gmail.com>
2021-02-03 11:20:54 +0000

    Create 08 - Two Timestamps.py

---
+++ 08 - Two Timestamps.py
@@ -0,0 +1,8 @@
+hoursOne = int(input())
+minutesOne = int(input())
+secondsOne = int(input())
+hoursTwo = int(input())
+minutesTwo = int(input())
+secondsTwo = int(input())
+
+print(hoursTwo * 3600 + minutesTwo * 60 + secondsTwo - hoursOne * 3600 - minutesOne * 60 - secondsOne)
